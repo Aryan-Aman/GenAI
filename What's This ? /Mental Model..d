8ab41cf1cd4f8a71d1fa0764bdd175f1672a1650
Aman <aman209001@gmail.com>
2025-02-17 15:02:13 +0530

    Delete What's This ? /Mental Model..d

--- What's This ? /Mental Model..d
+++
@@ -1,9 +0,0 @@
-# FOUNDATION MODELS: 
--generalized models-can perform many tasks -bcz too much data fed, many parameters etc.- eg LLMs 
-
-# GEN AI - divided into two parts Either build foundation models (Builder Perspective) or use foundation models (User Perspective).
-
-
-User_Perspective : Prompt Engineering,  RAG (Document chat), AI Agents, Vector Databases, Fine Tuning 
-
-Builder Perspective : RLHF(Reinforcement Learning with Human Feedback)-can be used to modify LLM behaviour used in building LLM, Pretraining (data given to train) , Quantization(Optimization of LLMs) , Fine Tuning
